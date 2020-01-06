716c94c66cb90027f602467d8108d80e3569bb56
Piotr Ferens <piotrferens@outlook.com>
2020-01-06 13:51:38 +0100

    Add input validation

--- src/app.js
+++ src/app.js
@@ -1,28 +1,51 @@
 import './assets/scss/app.scss'
 import $ from 'cash-dom'
 
+const searchInputValidation = /^[a-z0-9-_]{1,}$/
+
 export class App {
-    initializeApp() {
-        let self = this
+    constructor() {
+        this.searchInput = $('.username.input')
+        this.loadUserName = $('.load-username')
+        this.profileName = $('#profile-name')
+        this.profileImage = $('#profile-image')
+        this.profileUrl = $('#profile-url')
+        this.profileBio = $('#profile-bio')
 
-        $('.load-username').on('click', function(e) {
-            let userName = $('.username.input').val()
-
-            fetch('https://api.github.com/users/' + userName)
-                .then(response => response.json())
-                .then(function(body) {
-                    self.profile = body
-                    self.update_profile()
-                })
-        })
+        this.onLoadUserNameClick = this.onLoadUserNameClick.bind(this)
     }
 
-    update_profile() {
-        $('#profile-name').text($('.username.input').val())
-        $('#profile-image').attr('src', this.profile.avatar_url)
-        $('#profile-url')
-            .attr('href', this.profile.html_url)
-            .text(this.profile.login)
-        $('#profile-bio').text(this.profile.bio || '(no information)')
+    initializeApp() {
+        this.initializeEvents()
+    }
+
+    initializeEvents() {
+        this.loadUserName.on('click', this.onLoadUserNameClick)
+    }
+
+    fetchUser(userName) {
+        return fetch('https://api.github.com/users/' + userName).then(response => response.json())
+    }
+
+    onLoadUserNameClick() {
+        let userName = this.searchInput.val()
+        const isInputValid = searchInputValidation.test(userName)
+
+        if (isInputValid) {
+            this.searchInput.removeClass('is-danger')
+            this.fetchUser(userName).then(body => {
+                this.profile = body
+                this.updateProfile()
+            })
+        } else {
+            this.searchInput.addClass('is-danger')
+        }
+    }
+
+    updateProfile() {
+        this.profileName.text(this.searchInput.val())
+        this.profileImage.attr('src', this.profile.avatar_url)
+        this.profileUrl.attr('href', this.profile.html_url).text(this.profile.login)
+        this.profileBio.text(this.profile.bio || '(no information)')
     }
 }
